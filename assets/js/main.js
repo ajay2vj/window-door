5008a6addfe8dea96b0b801f31c1d9c9d2987ce3
ajay2vj <vjajay98@gmail.com>
2023-02-09 12:56:40 +0530

    gallery changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,32 +142,6 @@ major2Carousel.owlCarousel({
   }
 });
 
-  // gallery
-
-  $('.gallery-link').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    mainClass: 'mfp-with-zoom mfp-img-mobile',
-    image: {
-      verticalFit: true,
-      titleSrc: function(item) {
-        return item.el.find('figcaption').text() || item.el.attr('title');
-      }
-    },
-    zoom: {
-      enabled: true
-    },
-    // duration: 300
-    gallery: {
-      enabled: true,
-      navigateByImgClick: false,
-      tCounter: ''
-    },
-    disableOn: function() {
-      return $(window).width() > 640;
-    }
-  });
 
 /* 4. MainSlider-1 */
     // h1-hero-active
